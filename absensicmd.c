09af0c62705058530314afd7b3686d4c0ef8a263
syamsulary <maarief.syamsul12@gmail.com>
2016-05-19 06:48:59 +0700

    syamsul
    create nama dan absen

--- absensicmd.c
+++ absensicmd.c
@@ -21,6 +21,8 @@ main(){
     printf("Alief Here!!!\n\n");
     printf("%d",i[7]);
     printf("Chiqo Rifky Saputra\n\n");
+    printf("%d",i[30]);
+    printf("Syamsul Maarief\n\n");
     fungsiRamadhan();
     
     
