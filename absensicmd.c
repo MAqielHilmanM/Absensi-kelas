5cc23d454eafbcdbd36bfedc2c0d8c1a27816ca2
radhikayusuf <radhika.yusuf1@gmail.com>
2016-05-18 20:17:08 +0700

    Update absensicmd.c

--- absensicmd.c
+++ absensicmd.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 
 main(){
-    int i = 1;
-    int j = 2;
+    int i[32], j;
+    
+    for(j=0; j<32; j++){
+        i[j] = j+1;
+    }
+    
     printf("ABSENSI KELAS XI RPL 2\n\n\n\n");
 
-    printf("%d",i);
+    printf("%d",i[0]);
     printf("Muchamad Aqiel HM\n\n");
-    printf("%d",j);
+    printf("%d",i[1]);
     printf("Fajar Nazmi Fadillah\n\n");
+    printf("%d",i[2]);
+    printf("Radhika Yusuf Alifiansyah\n\n");
+    
 }
