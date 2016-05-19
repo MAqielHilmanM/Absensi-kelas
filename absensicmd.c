1e9076847e44b6b5a0062c1fed53226ad4686d33
M Aqiel Hilman M <maqiel2600@gmail.com>
2016-05-19 10:51:46 +0700

    Update absensicmd.c

--- absensicmd.c
+++ absensicmd.c
@@ -15,6 +15,7 @@ main(){
     */
     
     printf("ABSENSI KELAS XI RPL 2\n\n\n\n");
+
     printf("%d",i[17]);
     printf("Muchamad Aqiel HM\n\n");
     printf("%d",i[8]);
@@ -40,9 +41,9 @@ main(){
     printf("Fikri Adriansa Yudha\n\n");
     printf("%d", i[22]);
     printf("Muhammad Syahna \n\n");
-    printf("%d", i[27]);
+   
     
-    
+     printf("%d", i[27]);
     printf("Rizky Alfa Uji G\n\n");
     printf("%d", i[13]);
     printf("Iqbal Dzulfikar R\n\n");
