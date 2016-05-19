3743b249ee5881b3e381d94c0823aa14c3c5c611
bagasteguhpratista <bagaskawan@gmail.com>
2016-05-19 11:36:35 +0700

    Update absensicmd.c

--- absensicmd.c
+++ absensicmd.c
@@ -16,47 +16,138 @@ main(){
     
     printf("ABSENSI KELAS XI RPL 2\n\n\n\n");
 
-    printf("%d",i[17]);
-    printf("Muchamad Aqiel HM\n\n");
-    printf("%d",i[8]);
-    printf("Fajar Nazmi Fadillah\n\n");
-    printf("%d",i[23]);
-    printf("Radhika Yusuf Alifiansyah\n\n");
-    printf("%d",i[16]);
-    printf("Muhammad Angga was Here!!!\n\n");
-    printf("%d",i[3]);
+    //ABSEN 1
+    printf("%d",i[0]);
+    printf("Aditya Aria S\n\n");
+    
+    //ABSEN 2
+    
+    
+    
+    //ABSEN 3
+     printf("%d",i[2]);
     printf("Alief Here!!!\n\n");
+    
+    //ABSEN 4
+    
+    
+    
+    //ABSEN 5
+    
+    
+    
+    //ABSEN 6
+    
+    printf("%d", i[5]);
+    printf(" Bagas Teguh P\n\n");
+    
+    
+    //ABSEN 7
     printf("%d",i[6]);
     printf("Chiqo Rifky Saputra\n\n");
-    printf("%d",i[30]);
-    printf("Syamsul Maarief\n\n");
-    fungsiRamadhan();
-    printf("%d",i[7]);
+    
+    
+    //ABSEN 8
+     printf("%d",i[7]);
     printf("Dede Rusliandi\n\n");
-    printf("%d", i[32]);
-    printf("Vincentius Westley\n\n");
-    printf("%d",i[14]);
-    printf("Ivan Naufal Islami Al-faridzqi\n\n");
+    
+    //ABSEN 9
+    printf("%d",i[8]);
+    printf("Fajar Nazmi Fadillah\n\n");
+    
+    //ABSEN 10
+
+
+
+    //ABSEN 11
     printf("%d",i[10]);
     printf("Fikri Adriansa Yudha\n\n");
-    printf("%d", i[22]);
-    printf("Muhammad Syahna \n\n");
     
-    printf("%d", i[27]);
-    printf("Rizky Alfa Uji G\n\n");
+    //ABSEN 12
+    
+    
+    
+    //ABSEN 13
+    
+    
+    
+    //ABSEN 14
     printf("%d", i[13]);
     printf("Iqbal Dzulfikar R\n\n");
     
-    printf("%d", i[31]);
+    //ABSEN 15
+    printf("%d",i[14]);
+    printf("Ivan Naufal Islami Al-faridzqi\n\n");
+    
+    //ABSEN 16
+    
+    
+    
+    //ABSEN 17
+    printf("%d",i[16]);
+    printf("Muhammad Angga was Here!!!\n\n");
+    
+    //ABSEN 18
+    printf("%d",i[17]);
+    printf("Muchamad Aqiel HM\n\n");
+    
+    //ABSEN 19
+    
+    
+    
+    //ABSEN 20
+    
+    
+    //ABSEN 21
+    
+    fungsiRamadhan();
+    
+    //ABSEN 22
+    printf("%d", i[21]);
+    printf("Muhammad Syahna \n\n");
+    
+    //ABSEN 23
+    
+    
+    
+    //ABSEN 24
+     printf("%d",i[23]);
+    printf("Radhika Yusuf Alifiansyah\n\n");
+    
+    //ABSEN 25
+    
+    
+    
+    //ABSEN 26
+    
+    
+    
+    //ABSEN 27
+    printf("%d", i[26]);
+    printf("Rizky Alfa Uji G\n\n");
+    
+    //ABSEN 28
+    
+    
+    
+    //ABSEN 29
+    
+    
+    
+    //ABSEN 30    
+    printf("%d",i[29]);
+    printf("Syamsul Maarief\n\n");
+    
+    //ABSEN 31
+    printf("%d", i[30]);
     printf("Tendi F");
     
-    printf("%d", i[6]);
-    printf("Bagas Teguh P");
-    //testing//
-    //123//
+    //ABSEN 32
+    printf("%d", i[31]);
+    printf("Vincentius Westley\n\n");
+    
 }
 
 void fungsiRamadhan(){
     printf("Muhammad Ramadhan Rahmat Buat Fungsi Ruang Hampa");   
-}
 }
