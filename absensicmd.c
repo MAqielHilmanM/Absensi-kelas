bb389b1e0353a46527cca5dc4f7f74ecddd6e013
ivannaufal7 <ivannaufal7@gmail.com>
2016-05-19 08:16:15 +0700

    Update absensicmd.c

--- absensicmd.c
+++ absensicmd.c
@@ -28,6 +28,8 @@ main(){
     printf("Dede Rusliandi\n\n");
     printf("%d", i[32]);
     printf("Vincentius Westley\n\n");
+    printf("%d",i[15]);
+    printf("Ivan Naufal Islami A\n\n");
     
     
     
